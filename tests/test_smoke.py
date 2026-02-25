bbeb76aabfce0b4cd1ae942892da925b85aa1d0b
Chhavi-16 <vaishuu1608@gmail.com>
2026-02-25 20:23:47 +0530

    Update test_smoke.py
    Removed merge conflict markers from test_smoke.py.

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -1,9 +1,5 @@
-<<<<<<< HEAD
+
 def test_home_page_loads(client):
     res = client.get("/")
     assert res.status_code == 200
-=======
-def test_home_page_loads(client):
-    res = client.get("/")
-    assert res.status_code == 200
->>>>>>> c2e9f6021e5aa3f405794a69358b5d36c0350970
+
