6560f84b450326715f5bbf125011a73927cb5c70
Julliet-Mohanta <jullietmohanta@gmail.com>
2026-05-31 20:25:38 +0530

    Student login page change

--- app.py
+++ app.py
@@ -1606,8 +1606,6 @@ def export_achievement(achievement_id):
         return redirect(url_for("student-achievements"))
 
 
-if __name__ == "__main__":
-    init_db()
-    add_profile_picture_column()
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True, host='127.0.0.1', port=5001)
 
